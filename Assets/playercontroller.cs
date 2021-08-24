8c0864f7e087a80310b7febb03f4fbe980e4a3f6
peter <hguanqi7@gmail.com>
2021-08-25 05:59:50 +0800

    Basic Player Move

--- Assets/playercontroller.cs
+++ Assets/playercontroller.cs
@@ -7,12 +7,21 @@ public class playercontroller : MonoBehaviour
     // player: move, jump, collect drugs
     // advanced: crouch, sprint
     public int drugBar;
-    
+    public float movespeed, sprintspeed;
+    private float moveDir;
+    private Rigidbody2D rb;
+    private bool faceRight = true; //for 2d sprite
 
-
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
+        // Move func
+        moveDir = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveDir * movespeed, rb.velocity.y);
         
     }
 }
