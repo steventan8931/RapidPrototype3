00a306ffe61e395c297e4f37809233dafd9f3b2b
peter <hguanqi7@gmail.com>
2021-08-25 05:52:19 +0800

    Init upload

---
+++ Assets/playercontroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontroller : MonoBehaviour
+{
+    // player: move, jump, collect drugs
+    // advanced: crouch, sprint
+    public int drugBar;
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
