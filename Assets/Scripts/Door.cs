f669ce097b3102cb4ff55c1b8a2330354d084ae5
Steven Tan <tan2001steven@hotmail.com>
2021-08-25 16:21:25 +1200

    Door Edit

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,39 +5,21 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public bool m_IsOpen;
-    public Vector3 m_StartPosition;
-    public Vector3 m_EndPosition;
 
-    public float m_DoorOpenTimer = 0.0f;
-    public float m_DoorOpenSpeed = 0.5f;
-
-    bool m_CacheBool;
+    public GameObject m_OpenDoor;
+    public GameObject m_ClosedDoor;
 
     private void Update()
     {
-        if (m_CacheBool != m_IsOpen)
-        {
-            m_DoorOpenTimer = 0.0f;
-            m_CacheBool = m_IsOpen;
-        }
-
         if (m_IsOpen)
         {
-            if (transform.position != m_EndPosition)
-            {
-                m_DoorOpenTimer += Time.deltaTime * m_DoorOpenSpeed;
-            }
-
-            transform.position = Vector3.Lerp(transform.position, m_EndPosition, m_DoorOpenTimer);
+            m_OpenDoor.SetActive(true);
+            m_ClosedDoor.SetActive(false);
         }
         else
         {
-            if (transform.position != m_StartPosition)
-            {
-                m_DoorOpenTimer += Time.deltaTime * m_DoorOpenSpeed;
-            }
-
-            transform.position = Vector3.Lerp(transform.position, m_StartPosition, m_DoorOpenTimer);
+            m_OpenDoor.SetActive(false);
+            m_ClosedDoor.SetActive(true);
         }
     }
 }
