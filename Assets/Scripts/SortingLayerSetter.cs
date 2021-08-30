42fe8612de831ff954a9c1b58da6963964a5481d
Steven Tan <tan2001steven@hotmail.com>
2021-08-30 17:39:02 +1200

    Merged with Character (Added SortingLayerSetter) to work with 2D

---
+++ Assets/Scripts/SortingLayerSetter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SortingLayerSetter : MonoBehaviour
+{
+    public Renderer m_Renderer;
+
+    private void Update()
+    {
+        m_Renderer.sortingLayerName = "Player";
+    }
+}
