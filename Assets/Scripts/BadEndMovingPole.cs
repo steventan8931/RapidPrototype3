8f50cd4064517d5ffb22995bf102d6ff2cf26f35
Steven Tan <tan2001steven@hotmail.com>
2021-09-02 17:41:01 +1200

    BadEnd Scene Edit

---
+++ Assets/Scripts/BadEndMovingPole.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadEndMovingPole : MonoBehaviour
+{
+    public Vector3 m_StartPos;
+    public Vector3 m_EndPos;
+
+    private void Update()
+    {
+        if (transform.position != m_EndPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, m_EndPos, Time.deltaTime * 1.5f);
+
+        }
+        else
+        {
+            transform.position = m_StartPos;
+        }
+
+    }
+}
